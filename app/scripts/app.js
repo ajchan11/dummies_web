550aa6e7df580733f4edff1e9170b557619ac3b3
ajchan11 <ajchan11@gmail.com>
2015-11-10 11:43:48 -0800

    first commits

---
+++ app/scripts/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weirdsApp
+ * @description
+ * # weirdsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weirdsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/blog', {
+        templateUrl: 'views/blog.html',
+        controller: 'BlogCtrl',
+        controllerAs: 'blog'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .when('/team', {
+        templateUrl: 'views/team.html',
+        controller: 'TeamCtrl',
+        controllerAs: 'team'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
